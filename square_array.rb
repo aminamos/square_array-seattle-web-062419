2d44c4b4091b7dba8f648e03cc3d3d58a5e202fe
Amin Amos <aminamosahs@gmail.com>
2019-06-17 20:22:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
+<<<<<<< HEAD
   newarray = []
   array.each {|item| newarray << item**2}
   newarray
+=======
+  array.each {|item| array << item**2}
+>>>>>>> f88b7b2db1d3539d5c004c7c8511eee18de2f6a7
 end
